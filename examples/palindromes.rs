8433d1b9059b9179ab1eb460f5a8b8cfaaa5e202
Sam El-Kamand <sam.elkamand@gmail.com>
2026-06-18 03:16:49 +1000

    fix: palindrome script

--- examples/palindromes.rs
+++ examples/palindromes.rs
@@ -1,11 +1,11 @@
-use seqlib::sequence::IupacDnaSeq;
+use seqlib::sequence::{DnaSeq, IupacDnaSeq};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let palindrome = IupacDnaSeq::new("GAATTC")?;
+    let palindrome = DnaSeq::new("GAATTC")?;
     let ambiguous = IupacDnaSeq::new("NNNNNN")?;
 
     assert!(palindrome.is_palindromic());
-    assert!(!ambiguous.is_palindromic());
+    assert!(ambiguous.is_palindromic_checked().is_err());
 
     println!("{palindrome} is palindromic");
     println!("{ambiguous} is NOT palindromic (ambiguous)");
