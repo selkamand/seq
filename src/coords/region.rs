e9ddc5731f74626e713666fd868122f4d813e000
selkamand <sam.elkamand@gmail.com>
2026-07-05 22:59:05 +1000

    feat: added Region constructor

--- src/coords/region.rs
+++ src/coords/region.rs
@@ -5,8 +5,8 @@ use crate::coords::Interval;
 /// A region (name of originating sequence + 1-based inclusive interval)
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Region {
-    name: String,
-    interval: Interval,
+    pub name: String,
+    pub interval: Interval,
 }
 
 impl std::fmt::Display for Region {
@@ -16,5 +16,19 @@ impl std::fmt::Display for Region {
             "{}:{}-{}",
             self.name, self.interval.start, self.interval.end
         )
+    }
+}
+
+impl Region {
+    /// Create a new [`Region`] from a sequence **name** and [`Interval`]
+    pub fn new<N, I>(name: N, interval: I) -> Self
+    where
+        N: Into<String>,
+        I: Into<Interval>,
+    {
+        Self {
+            name: name.into(),
+            interval: interval.into(),
+        }
     }
 }
