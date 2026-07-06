8f384b12ccf48a47e21ac0b03887ed1145e4e66e
selkamand <sam.elkamand@gmail.com>
2026-07-06 15:26:19 +1000

    refactor!: made region fields private w/ getters

--- src/coords/region.rs
+++ src/coords/region.rs
@@ -5,8 +5,8 @@ use crate::coords::Interval;
 /// A region (name of originating sequence + 1-based inclusive interval)
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Region {
-    pub name: String,
-    pub interval: Interval,
+    name: String,
+    interval: Interval,
 }
 
 impl std::fmt::Display for Region {
@@ -30,5 +30,15 @@ impl Region {
             name: name.into(),
             interval: interval.into(),
         }
+    }
+
+    /// Get region [`Interval`]
+    pub fn interval(&self) -> &Interval {
+        &self.interval
+    }
+
+    /// Name of sequence interval is from (e.g. chromosome / contig /etc)
+    pub fn name(&self) -> &str {
+        &self.name
     }
 }
